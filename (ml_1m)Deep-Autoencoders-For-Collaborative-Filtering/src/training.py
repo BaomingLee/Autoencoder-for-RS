cb959a0381082532f930f7220c1ca4f93532a1ad
BaomingLee <44024527+BaomingLee@users.noreply.github.com>
2019-08-14 14:24:09 +0100

    Update training.py

--- (ml_1m)Deep-Autoencoders-For-Collaborative-Filtering/src/training.py
+++ (ml_1m)Deep-Autoencoders-For-Collaborative-Filtering/src/training.py
@@ -20,7 +20,7 @@ tf.app.flags.DEFINE_string('tf_records_test_path', os.path.abspath(os.path.join(
 tf.app.flags.DEFINE_string('checkpoints_path', os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'checkpoints/model.ckpt')), 
                            'Path for the test data.')
 
-tf.app.flags.DEFINE_integer('num_epoch', 200,
+tf.app.flags.DEFINE_integer('num_epoch', 20,
                             'Number of training epochs.')
 
 tf.app.flags.DEFINE_integer('batch_size', 16,
