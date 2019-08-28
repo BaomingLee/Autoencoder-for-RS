776cf5837d3fa039c4df4a87c2e4cc621435bced
BaomingLee <44024527+BaomingLee@users.noreply.github.com>
2019-08-28 15:46:39 +0100

    one

--- (ml_100k)Deep-Autoencoders-For-Collaborative-Filtering/src/model/base_model.py
+++ (ml_100k)Deep-Autoencoders-For-Collaborative-Filtering/src/model/base_model.py
@@ -22,38 +22,38 @@ class BaseModel(object):
         '''
 
         
-        with tf.name_scope('weights'):
-            self.W_1=tf.get_variable(name='weight_1', shape=(self.FLAGS.num_v,256), 
-                                     initializer=self.weight_initializer)
-            self.W_2=tf.get_variable(name='weight_2', shape=(256,128), 
-                                     initializer=self.weight_initializer)
-            self.W_3=tf.get_variable(name='weight_3', shape=(128,256), 
-                                     initializer=self.weight_initializer)
-            self.W_4=tf.get_variable(name='weight_4', shape=(256,self.FLAGS.num_v), 
-                                     initializer=self.weight_initializer)
-        
-        with tf.name_scope('biases'):
-            self.b1=tf.get_variable(name='bias_1', shape=(256), 
-                                    initializer=self.bias_initializer)
-            self.b2=tf.get_variable(name='bias_2', shape=(128), 
-                                    initializer=self.bias_initializer)
-            self.b3=tf.get_variable(name='bias_3', shape=(256), 
-                                    initializer=self.bias_initializer)
-
-
         # with tf.name_scope('weights'):
         #     self.W_1=tf.get_variable(name='weight_1', shape=(self.FLAGS.num_v,256), 
         #                              initializer=self.weight_initializer)
-        #     self.W_2=tf.get_variable(name='weight_2', shape=(256,256), 
+        #     self.W_2=tf.get_variable(name='weight_2', shape=(256,128), 
         #                              initializer=self.weight_initializer)
-        #     self.W_3=tf.get_variable(name='weight_3', shape=(256,self.FLAGS.num_v), 
+        #     self.W_3=tf.get_variable(name='weight_3', shape=(128,256), 
+        #                              initializer=self.weight_initializer)
+        #     self.W_4=tf.get_variable(name='weight_4', shape=(256,self.FLAGS.num_v), 
         #                              initializer=self.weight_initializer)
         
         # with tf.name_scope('biases'):
         #     self.b1=tf.get_variable(name='bias_1', shape=(256), 
         #                             initializer=self.bias_initializer)
-        #     self.b2=tf.get_variable(name='bias_2', shape=(256), 
+        #     self.b2=tf.get_variable(name='bias_2', shape=(128), 
         #                             initializer=self.bias_initializer)
+        #     self.b3=tf.get_variable(name='bias_3', shape=(256), 
+        #                             initializer=self.bias_initializer)
+
+
+        with tf.name_scope('weights'):
+            self.W_1=tf.get_variable(name='weight_1', shape=(self.FLAGS.num_v,256), 
+                                     initializer=self.weight_initializer)
+            self.W_2=tf.get_variable(name='weight_2', shape=(256,256), 
+                                     initializer=self.weight_initializer)
+            self.W_3=tf.get_variable(name='weight_3', shape=(256,self.FLAGS.num_v), 
+                                     initializer=self.weight_initializer)
+        
+        with tf.name_scope('biases'):
+            self.b1=tf.get_variable(name='bias_1', shape=(256), 
+                                    initializer=self.bias_initializer)
+            self.b2=tf.get_variable(name='bias_2', shape=(256), 
+                                    initializer=self.bias_initializer)
 
 
 
@@ -62,22 +62,22 @@ class BaseModel(object):
 
 
     
-    def inference(self, x):
+    # def inference(self, x):
 
-        '''
-        Making one forward pass. Predicting the networks outputs, 
-        given the inputs sample x (a row of the user-movie matrix).
+    #     '''
+    #     Making one forward pass. Predicting the networks outputs, 
+    #     given the inputs sample x (a row of the user-movie matrix).
        
-        @param x: input ratings
-        @return : networks predictions
-        '''
+    #     @param x: input ratings
+    #     @return : networks predictions
+    #     '''
         
-        with tf.name_scope('inference'):
-             a1=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(x, self.W_1),self.b1))
-             a2=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(a1, self.W_2),self.b2))
-             a3=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(a2, self.W_3),self.b3))   
-             a4=tf.matmul(a3, self.W_4) 
-        return a4
+    #     with tf.name_scope('inference'):
+    #          a1=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(x, self.W_1),self.b1))
+    #          a2=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(a1, self.W_2),self.b2))
+    #          a3=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(a2, self.W_3),self.b3))   
+    #          a4=tf.matmul(a3, self.W_4) 
+    #     return a4
 
 
 
@@ -101,18 +101,18 @@ class BaseModel(object):
     #     return a4
 
 
-    # def inference(self, x):
+    def inference(self, x):
 
-    #     '''
-    #     Making one forward pass. Predicting the networks outputs, 
-    #     given the inputs sample x (a row of the user-movie matrix).
+        '''
+        Making one forward pass. Predicting the networks outputs, 
+        given the inputs sample x (a row of the user-movie matrix).
        
-    #     @param x: input ratings
-    #     @return : networks predictions
-    #     '''
+        @param x: input ratings
+        @return : networks predictions
+        '''
 
-    #     with tf.name_scope('inference'):
-    #          a1=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(x, self.W_1),self.b1))
-    #          a2=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(a1, self.W_2),self.b2))  
-    #          a3=tf.matmul(a2, self.W_3) 
-    #     return a3
+        with tf.name_scope('inference'):
+             a1=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(x, self.W_1),self.b1))
+             a2=tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(a1, self.W_2),self.b2))  
+             a3=tf.matmul(a2, self.W_3) 
+        return a3
